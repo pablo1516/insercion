be9f99660fdaca336a3691f5d13c21e271dad520
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-04-10 19:25:41 -0500

    Add files via upload

---
+++ Ordenamiento por insercion/src/ordenamiento/por/insercion/OrdenamientoPorInsercion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordenamiento.por.insercion;
+
+/**
+ *
+ * @author david
+ */
+public class OrdenamientoPorInsercion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmInsercion frminser = new FrmInsercion();
+        frminser.setVisible(true);
+        frminser.setLocationRelativeTo(null);
+        frminser.setSize(400,300);
+        frminser.setResizable(false);
+        frminser.setTitle("Ordenamiento por Insercion");
+        
+    }
+    
+}
